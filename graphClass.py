ffdee3da221744160e45ae5dbd40ed26be9be04e
Max <max.oblaender@uni-jena.de>
2022-01-30 19:09:20 +0100

    add get_adjacent and add_adjacent

--- graphClass.py
+++ graphClass.py
@@ -1,21 +1,29 @@
 class Vertex():
     """Klasse um die Vertices des Graphen darzustellen"""
 
-    def __init__(self, lable):
+    def __init__(self, label):
         """Konstruktor setzt den Seitennamen als label"""
-        self.lable = lable
-        self.children = set()
+        self.label = label
+        self.adjacent = set()
 
-    def add_neighbour(self,neighbour):
+    #def __str__(self):
+     #   return str(self.label) + "adjacent:" + str([v.label for v in self.adjacent])
+
+    def add_adjacent(self,neighbour):
         """Fühgt eine Kante zu einem gegebenen Vertex hinzu"""
-        self.children.add(neighbour)
+        if(neighbour in self.adjacent):
+            #noch schauen, ob neighbour schon in set
+            return False
+        else:
+            self.adjacent.add(neighbour)
+            return True
 
-    def get_lable(self):
-        return self.lable
+    def get_label(self):
+        return self.label
 
-    def get_children(self):
+    def get_adjacent(self):
         """Gibt alle verbundenen Vertices von diesem Vertex zurück"""
-        return self.children
+        return self.adjacent
 
 class Graph():
     """Klasse die den Graphen darstellt"""
@@ -28,21 +36,21 @@ class Graph():
     def __iter__(self):
         return iter(self.vertices_dict.values())
 
-    def add_vertex(self,lable):
+    def add_vertex(self,label):
         """Fügt neues Vertex ein. Setzt keine Kanten"""
-        new_vertex = Vertex(lable)
+        new_vertex = Vertex(label)
         self.num_vertices = self.num_vertices +1
-        self.vertices_dict[lable] = new_vertex
+        self.vertices_dict[label] = new_vertex
         #return new_vertex
 
-    def get_vertex(self,lable):
+    def get_vertex(self,label):
         """Holt den Vertice mit dem gesuchten Seitennamen aus dem Dictionary"""
-        if(lable in self.vertices_dict):
-            return self.vertices_dict[lable]
+        if(label in self.vertices_dict):
+            return self.vertices_dict[label]
         else:
             pass #vielleicht noch abfangen
 
-    def add_edge(self,lable_start,lable_end):
-        if(lable_start and lable_end in self.vertices_dict):
-            self.vertices_dict[lable_start].add_neighbour(self.vertices_dict[lable_end])
-            #self.vertices_dict[lable_end].add_neighbour(self.vertices_dict[lable_start])
+    def add_edge(self,label,label_end):
+        if(label and label_end in self.vertices_dict):
+            self.vertices_dict[label].add_adjacent(self.vertices_dict[label_end])
+            #self.vertices_dict[label_end].add_neighbour(self.vertices_dict[label_start])
