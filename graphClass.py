93eb5124f784e35613f1186d9e252252a5d7bea1
Max <max.oblaender@uni-jena.de>
2022-03-26 09:23:18 +0100

    added more graph functions

--- graphClass.py
+++ graphClass.py
@@ -1,29 +1,32 @@
+
 class Vertex():
     """Klasse um die Vertices des Graphen darzustellen"""
 
     def __init__(self, label):
         """Konstruktor setzt den Seitennamen als label"""
         self.label = label
-        self.adjacent = set()
+        self.children = set() #gerichtet ist hier immer von sich selbst zu adjacent
+        self.parents = set()
 
     #def __str__(self):
      #   return str(self.label) + "adjacent:" + str([v.label for v in self.adjacent])
 
-    def add_adjacent(self,neighbour):
+    def add_child(self,child): #fügt Kante von sich selbst zum Nachbarn hin hinzu
         """Fühgt eine Kante zu einem gegebenen Vertex hinzu"""
-        if(neighbour in self.adjacent):
-            #noch schauen, ob neighbour schon in set
-            return False
-        else:
-            self.adjacent.add(neighbour)
-            return True
+        self.children.add(child)
+    
+    def add_parent(self,parent):
+        self.parents.add(parent)
+
+    def get_children(self):
+        return self.children
+
+    def get_parents(self):
+        """Gibt alle verbundenen Vertices von diesem Vertex zurück"""
+        return self.parents
 
     def get_label(self):
         return self.label
-
-    def get_adjacent(self):
-        """Gibt alle verbundenen Vertices von diesem Vertex zurück"""
-        return self.adjacent
 
 class Graph():
     """Klasse die den Graphen darstellt"""
@@ -32,25 +35,74 @@ class Graph():
         """Konstruktor. Vertices werden in einem Dictionary gespeichert"""
         self.vertices_dict = {}
         self.num_vertices = 0
+        self.num_edges = 0
 
     def __iter__(self):
         return iter(self.vertices_dict.values())
 
     def add_vertex(self,label):
-        """Fügt neues Vertex ein. Setzt keine Kanten"""
-        new_vertex = Vertex(label)
-        self.num_vertices = self.num_vertices +1
-        self.vertices_dict[label] = new_vertex
-        #return new_vertex
+        """Fügt neuen Vertex ein. Setzt keine Kanten"""
+        if not label in self.vertices_dict:
+            self.num_vertices = self.num_vertices +1
+            new_vertex = Vertex(label) #muss prüfen, ob es schon einen Vertex mit dem Label gibt (macht das ein dict schon? ja ne?)
+            self.vertices_dict[label] = new_vertex
+            print(label)
+        else:
+            print(label," was already found!")
 
     def get_vertex(self,label):
-        """Holt den Vertice mit dem gesuchten Seitennamen aus dem Dictionary"""
+        """Holt den Vertix mit dem gesuchten Seitennamen aus dem Dictionary"""
         if(label in self.vertices_dict):
             return self.vertices_dict[label]
         else:
             pass #vielleicht noch abfangen
 
-    def add_edge(self,label,label_end):
-        if(label and label_end in self.vertices_dict):
-            self.vertices_dict[label].add_adjacent(self.vertices_dict[label_end])
-            #self.vertices_dict[label_end].add_neighbour(self.vertices_dict[label_start])
+    def add_edge(self,label_parent,label_child):
+        """Fügt Kante zwischen label und label_end über eine Vertex-Methode ein."""
+        if(label_parent and label_child in self.vertices_dict):
+            self.vertices_dict[label_parent].add_child(self.vertices_dict[label_child])
+            self.vertices_dict[label_child].add_parent(self.vertices_dict[label_parent])
+            self.num_edges = self.num_edges + 1
+
+    def get_neighbours(self,vertex): #was wenn gleichzeitig parent und child??
+        """Abfragen alle Nachbarknoten"""
+        neighbors = set()
+        for parent in vertex.get_parents():
+            neighbors.add(parent)
+        for child in vertex.get_children():
+            neighbors.add(child)
+        return neighbors
+
+    def get_highest_entry_degree(self):
+        """Abfrage des Knotens mit den meisten Eltern"""
+        highest_vertex = None
+        number = 0
+        for vertex in self.vertices_dict:
+            amount = len(vertex.get_parents())
+            if amount > number:
+                highest_vertex = vertex
+        return highest_vertex
+
+    def get_highest_exit_degree(self):
+        """Abfrage des Knotens mit den meisten Kindern"""
+        highest_vertex = None
+        number = 0
+        for vertex in self.vertices_dict:
+            amount = len(vertex.get_children())
+            if amount > number:
+                highest_vertex = vertex
+        return highest_vertex
+
+    def get_density(self):
+        E = self.num_edges
+        V = self.num_vertices
+        return E/(V*(V-1))
+
+    def __str__(self): #nicht wirklich richtig
+        output = []
+        for vertex in self.vertices_dict.items(): 
+            output.append(vertex.get_label())
+            output.append("\n")
+            for child in vertex.get_children():
+                output.append(child.get_label())
+        return str(output)
