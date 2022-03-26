d7df7df3d596cde281661845f3ac0081857a6986
Max <max.oblaender@uni-jena.de>
2022-03-26 09:23:40 +0100

    finished run() method

--- queryClass.py
+++ queryClass.py
@@ -1,40 +1,85 @@
+import queue
 import graphClass
 import requests
 import re
+import drawClass
 
 class Query():
-    """Klasse zum holen und verarbeiten der HTML-Seiten"""
+    """Klasse zum holen und verarbeiten der HTML-Seiten. Hat einen Graphen als (Klassenattribut)"""
     g = graphClass.Graph
-    def __init__(self,graph):           
+
+    def __init__(self,url,graph,max_amount=500,max_depth=10):
+        """setzt den Graphen"""
         self.g = graph
-        self.first = True
+        self.queue = [] # die liste wird als quasi queue verwendet um die Tiefe richtig zu zählen
+        self.max_amount = max_amount #max anzahl an Seiten
+        self.max_depth = max_depth  #max anzahl an Tiefe
+        self.run(url,0,0)
 
     def find_titel(self,url):
-        """Setzt den titel(label)"""
-        self.HTML = requests.get(url).text
-        tmp_title_list = re.findall("<title>.*-",self.HTML)
+        """Setzt den titel(label) aus der HTML Seite"""
+        try:
+            HTML = requests.get(url).text
+        except:
+            HTML = requests.get("https://en.wikipedia.org/wiki/"+url).text
+        tmp_title_list = re.findall("<title>.*-",HTML)
         try:    #falls kein Titel gefunden werden kann
             tmp_title = tmp_title_list[0] 
         except:
-            print("no titel--------------------------------------------------------------")
-            return
+            print("no title in",url)
+            return "no title found"
         tmp_title = re.sub("<title>","",tmp_title)
-        return re.sub("\s-","",tmp_title)       
+        return re.sub("\s-","",tmp_title) 
 
-    def find_all_links(self,url,last_label,depth):
-        #findet alle "a href" und setzt eigenen titel
-        self.HTML = requests.get(url).text 
-        
+    def find_all_links(self,label):
+        """findet alle weiterführenden Links"""
+        HTML = requests.get("https://en.wikipedia.org/wiki/"+label).text #holt html von Seite
+        #holt die nächsten Links aus der aktuellen HTML Seite
+        all_links = re.findall("a\shref=\"\/wiki\/.*?\"",HTML) 
+        proper_links = []
+        for entry in all_links:    
+            if (not re.search(r":",entry) and not re.search("Main\_Page",entry)): #entfernt Links, die nicht auf "richtige" wiki Seiten verweisen
+                tmp = re.sub("a\shref=\"\/wiki\/","",entry)
+                tmp = re.sub("\"","",tmp)
+                proper_links.append(tmp)
+        return proper_links  
 
-        #es soll sich last_label nur ändern, wenn depth sich geändert hat
-        #bzw. last_rekusion_level
-        
+    def run(self,url,depth,amount):
+        """Hauptmethode für die Iteration"""
+        labelroot = self.find_titel(url) #findet titel der ersten Seite
+        g.add_vertex(labelroot) #fügt Wurzel in Graphen g
+        self.queue.append(labelroot) #fügt Wurzel in die queue
+        amount = amount +1 #K
+        self.queue.append("$") #fügt (besonderes) Element in die queue, welches aufzeigt, wenn nächste Tiefe erreicht wird
 
-        
-        self.label = self.find_titel(url)
+        while depth < self.max_depth and amount < self.max_amount:
+            print("amount: ",amount)
+            if self.queue[0] == "$": #nur an erster Stelle in queue, wenn neuer Abstand von Wurzel erreicht wird
+                depth = depth + 1
+                self.queue.append("$") #enqueue
+                self.queue.pop(0) #dequeue
+                print("new depth")
 
-        
+            all_links = self.find_all_links(self.queue[0])
 
+            for link in all_links:
+                if amount < self.max_amount:
+                    self.queue.append(link) #fügt gefundenen Link in die queue ein
+                    titel = self.find_titel("https://en.wikipedia.org/wiki/"+link) 
+                    g.add_vertex(titel)
+                    g.add_edge(self.find_titel(self.queue[0]),titel) #fügt Kante von dem ersten element der Queue (Elter der aktuellen Seite) zur aktuellen Seite hinzu
+                    amount = amount + 1
+            
+            self.queue.pop(0) #Link an erster Stelle der queue wurde abgearbeitet
+            print("----------------------pop-----------------")
+
+        print("finished with: ",amount," ",depth)
+        self.draw(self.g)
+
+    def draw(self,graph):
+        """Methode für die darstellung des Graphen. Benutzt die Klasse Draw"""
+        draw_class = drawClass.Draw(graph)
+        draw_class.draw()
 
         # g.add_vertex(self.label)            #fügt den aktuellen Titel als Knoten in den Bau        
 
@@ -49,43 +94,53 @@ class Query():
         #     self.first = False
         
         
-        #muss keine titel finden, da das im nächsten schritt gemacht wird
+        # #muss keine titel finden, da das im nächsten schritt gemacht wird
 
-        #noch die Abfrage, ob noch mal Rekursion
-        if(depth<3):
+        # #noch die Abfrage, ob noch mal Rekursion
+        # if(depth < self.max_depth and amount < self.max_amount): #hier nur depth
 
-            #holt die nächsten Links aus der aktuellen HTML Seite
-            tmp_links_list = re.findall("a\shref=\"\/wiki\/.*?\"",self.HTML)
-            links_list2 = []
-            for entry in tmp_links_list:    
-                if (not re.search(r":",entry)): #entfernt Links, die nicht auf "richtige" wiki Seiten verweisen
-                    tmp = re.sub("a\shref=\"","",entry)
-                    links_list2.append(tmp)
-            links_list = []
-            for entry in links_list2:
-                tmp = re.sub("\"","",entry)
-                links_list.append(tmp) #links_list ist die Liste mit den neuen Links
+        #     #holt die nächsten Links aus der aktuellen HTML Seite
+        #     tmp_links_list = re.findall("a\shref=\"\/wiki\/.*?\"",self.HTML)
+        #     links_list2 = []
+        #     for entry in tmp_links_list:    
+        #         if (not re.search(r":",entry)): #entfernt Links, die nicht auf "richtige" wiki Seiten verweisen
+        #             tmp = re.sub("a\shref=\"","",entry)
+        #             links_list2.append(tmp)
+        #     links_list = []     #kann man das nicht in eine Schleife packen?
+        #     for entry in links_list2:
+        #         tmp = re.sub("\"","",entry)
+        #         links_list.append(tmp) #links_list ist die Liste mit den neuen Links
 
-            depth = depth+1
-            self.first = True
+        #     depth = depth+1 #stimmt nicht unbedingt, da ja nur depth für hier diesen zweig
+        #     print("depth: ",depth)
             
-            g.add_vertex(self.label)
-            node = g.get_vertex(self.label)
+        #     g.add_vertex(self.label) #fügt aktuelle seite als Knoten in den Graphen g
+        #     print(self.label)
+        #     node = g.get_vertex(self.label) #ist das aktuelle Vertex element
 
-            #rekursions Schritt
-            i = 0
-            for link in links_list:
-                if i<5:
-                    new_label = self.find_titel("https://en.wikipedia.org"+link)
-                    node.add_adjacent(new_label)
+        #     #rekursions Schritt
+        #     #amount = amount + len(links_list) #das muss noch richtig überarbeitet werden. Vielleicht in der for schleife jedes mal hoch zählen und keinen aufruf mehr machen
+        #     # print("amount",amount)
+        #     next_links = []
+        #     for link in links_list:
+        #         if amount < self.max_amount: #hier noch mal amount < 500 abfragen
+        #             new_label = self.find_titel("https://en.wikipedia.org"+link) #durchsucht das englische Wikipedia
+        #             node.add_adjacent(new_label)
+        #             amount = amount+1 #vor oder nach dem nächsten rekursionsaufruf? glaube hier schon richtig
+        #             next_links.append(link)
+        #                     #print("label",self.label,depth)
+        #                     #print("---------------------------node------------------",node.get_label())
+        #             # e = node.get_label() #was hier???????????????????
+        #                     #print(f"Kante: {new_label} -> {e}")
+        #             # self.find_all_links("https://en.wikipedia.org"+link,last_label,depth,amount) #last_label noch relevant?  ne
+                    
+        #             print("amount:",amount)  
+        #     for link in next_links:
+        #         self.run("https://en.wikipedia.org"+link,last_label,depth,amount)
 
-                    #print("label",self.label,depth)
-                    #print("---------------------------node------------------",node.get_label())
-                    e = node.get_label()
-                    #print(f"Kante: {new_label} -> {e}")
-                    self.find_all_links("https://en.wikipedia.org"+link,last_label,depth)    
-                    i = i+1
-                
+        
+                   
+                    
             
 
 
@@ -104,32 +159,31 @@ if __name__ == "__main__":
     
     #https://en.wikipedia.org noch davor
     #https://en.wikipedia.org/wiki/Valuative_criterion
-    #url = "https://en.wikipedia.org/wiki/Valuative_criterion"
-    url ="https://en.wikipedia.org/wiki/Newick_format"
-    #url = "https://en.wikipedia.org/wiki/Sursurunga_language"
-    #url = "https://en.wikipedia.org/wiki/Germany"
-    #url = "https://en.wikipedia.org/wiki/Chris_Alp"
-    K=500
-    D=10
-    i=0
-    j=0
-    #while(i<=K and j<=D):
-    q = Query(g)
-    q.find_all_links(url,None,0)
+    # url = "https://en.wikipedia.org/wiki/Valuative_criterion"
+    # url ="https://en.wikipedia.org/wiki/Newick_format"
+    # url = "https://en.wikipedia.org/wiki/Germany"
+    # url = "https://en.wikipedia.org/wiki/Pierre-Jules_Cavelier"
+    url = "https://en.wikipedia.org/wiki/Chris_Alp"
+    # url = "https://en.wikipedia.org/wiki/Tacoma_Narrows_Bridge"
 
-    print()
-    for v in g:
-        print(v.get_label())
-        for n in v.get_adjacent():
-            a = v.get_label()
-            b = n
-            print(f"parent: {a}, nachbar: {b}")
+    q = Query(url,g,50,8)
+    
+    print(g)
+
+    # print()
+    # for v in g:
+    #     print(v.get_label())
+    #     for n in v.get_adjacent():
+    #         a = v.get_label()
+    #         b = n
+    #         print(f"parent: {a}, nachbar: {b}")
 
     
-    for e in g:
-        print(e.get_label())
+    #print(g)
 
 
-#bugs :
-# am ender einer Rekursion findet er keinen Titel
-#
+#todo:
+# 1. ausgabe des Graphen als txt form
+# 2. commandline tool -> -h usw
+# 3. Zusatzaufgabe: Eingabe string Node rot unterlegen
+# 3. Bildausgabe schöner machen
